7ae27159be995350fdc1260b8f90215833f98397
Timbusien <timbusiennavi@gmail.com>
2023-11-27 00:51:32 +0500

    update 2.1

--- database/skinservice.py
+++ database/skinservice.py
@@ -29,7 +29,50 @@ def edit_skin_my_db(skin_id, edit_type, new_data):
             exact_skin.exterior = new_data
         my_db.commit()
 
-        return 'skin info changed successfully'
+        return 'Skin info changed successfully'
     else:
         return 'Skin in not Found'
+
+
+def add_skin_photo_my_db(skin_id, skin_photo):
+    my_db = next(get_database())
+    new_skin_photo = SkinPhoto(skin_id=skin_id, skin_photo=skin_photo)
+
+    my_db.add(new_skin_photo)
+    my_db.commit()
+
+    return 'Photo of skin successfully added'
+
+
+def delete_skin_my_db(skin_id):
+    my_db = next(get_database())
+    check_skin = my_db.query(Skin).filter_by(skin_id=skin_id).first()
+    skin_photo_check = my_db.query(SkinPhoto).filter_by(skin_id=skin_id).first()
+
+    if check_skin:
+        my_db.delete(check_skin)
+        my_db.commit()
+        my_db.delete(skin_photo_check)
+        my_db.commit()
+
+    return 'Skin deleted successfully'
+
+
+def get_exact_user_skin(user_id):
+    my_db = next(get_database())
+    exact_user_skin = my_db.query(Skin).filter_by(user_id=user_id).first()
+
+    return exact_user_skin
+
+
+def get_exact_skin(user_id):
+    my_db = next(get_database())
+    exact_skin = my_db.query(Skin).filter_by(user_id=user_id).first()
+
+    if exact_skin:
+        return exact_skin
+    else:
+        return False
+
+
 
